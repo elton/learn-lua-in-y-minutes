f04775415c9c8715431f5393ecb0d9eb8771ff3b
Elton Zheng <elton.zheng.sh@gmail.com>
2022-05-26 12:27:29 +0800

    initialize project.

---
+++ lua.lua
@@ -0,0 +1,15 @@
+-- Copyright (c) 2022 Elton Zheng
+--
+-- This software is released under the MIT License.
+-- https://opensource.org/licenses/MIT
+
+-- 单行注释以两个连字符开头
+
+--[[
+     多行注释
+--]]
+
+----------------------------------------------------
+-- 1. 变量和流程控制
+----------------------------------------------------
+
